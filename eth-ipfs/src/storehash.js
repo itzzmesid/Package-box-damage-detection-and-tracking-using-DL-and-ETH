467171abe0f9fa0394aab4caeedc9bbb6efef77f
S Sidharth <43806972+itzzmesid@users.noreply.github.com>
2022-06-27 23:20:47 +0530

    Add files via upload

---
+++ eth-ipfs/src/storehash.js
@@ -0,0 +1,37 @@
+import web3 from './web3';
+
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+const address = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+//use the ABI from your contract
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
